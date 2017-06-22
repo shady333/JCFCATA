892f68e4db435b723b34efb39f1b19bbda7eac9b
Oleg_Dudar <oleg.dudar@gmail.com>
2017-06-22 17:23:38 +0300

    Added test report

--- src/test/java/CreatorTest.java
+++ src/test/java/CreatorTest.java
@@ -1,6 +1,7 @@
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.*;
@@ -22,6 +23,7 @@ public class CreatorTest {
     @Test
     public void testCreateJson() throws Exception {
         System.out.println("Test");
+
         //TODO please remove this!!!
         Assert.assertTrue(true);
 
